f3d67b81a162c56f26d34e96f090cc5dc9a1d448
svsk <svsk@DESKTOP-E3DUNEN>
2023-01-10 14:23:15 +0530

    third

--- jenkins/src/main/java/demo/Test3.java
+++ jenkins/src/main/java/demo/Test3.java
@@ -4,12 +4,11 @@ import org.testng.annotations.Test;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
-public class Test1{
-	
+public class Test3 {
 	@Test
-	public void m1()
+	public void m3()
 	{
-	 Reporter.log("hello", true);
+		Reporter.log("SMILE",true);
 	}
 
 }
